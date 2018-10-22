656d684ae929873d5f9ca9800eb617789b997e37
Antti Koponen <koponen942@outlook.com>
2018-10-23 00:24:21 +0300

    Signing?? No?

--- DiscordBot/bot.js
+++ DiscordBot/bot.js
@@ -1,19 +1,23 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const prefix = "!";
 
 client.on("ready", () => {
     console.log("I am ready!");
 });
 
 client.on("message", (message) => {
-    if (message.content.startsWith("ping")) {
+    if (!message.content.startsWith(prefix)) return;
+
+    if (message.content.startsWith(prefix + "ping")) {
         message.channel.send("pong!");
     } else
 
-        if (message.content.startsWith("foo")) {
+        if (message.content.startsWith(prefix + "foo")) {
             message.channel.send("bar!");
         }
 });
+
 
 client.login("Mzc5MDM3OTk5OTU1OTY4MDI5.Dq_Hlg.n4AW-F5JzOO5Kx9DgqZnA-su0ss");
 
